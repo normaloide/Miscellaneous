f22360cf5efd054fbe762421867935eec48820f6
Edoardo Ferrari <43927507+normaloide@users.noreply.github.com>
2018-11-26 15:10:18 +0100

    Update Spam.js

--- JavaScript/Telegram/Spam.js
+++ JavaScript/Telegram/Spam.js
@@ -1,6 +1,13 @@
-var testoinizio = "init_text";
+/**
+* Note that even if it's called "Spam.js", Telegram will automatically
+* detect excessive spam and will block you for a n period of time.
+* This program is in fact intended for automatization of messages that
+* have to be sent every n time.
+*/
+
+var testoinizio = "init_text";  //this text will be executed once
 var testo = "text";
-var intervallo = 1;
+var intervallo = 1;  //seconds
 var volte = 100;
 document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = testoinizio;
 $('.im_submit').trigger('mousedown'); 
